b5b930edab3e8b25cb9b693b13c952bb39612776
yjohnson67 <joh23004@byui.edu>
2025-05-02 09:33:06 -0500

    Created contacts.json

--- backend/app.js
+++ backend/app.js
@@ -14,4 +14,6 @@ app.use((req, res, next) => {
 
 app.use('/professional', professionalRoutes);
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log('server is running on http://localhost:8080');
+});
